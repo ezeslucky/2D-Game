c32125f01d76f602ab6e98b62853fd90fc59f661
Lucky <lucky123@gmail.com>
2024-11-23 18:43:56 +0530

    stage 2 http server

--- metaverse/apps/http/src/routes/v1/space.ts
+++ metaverse/apps/http/src/routes/v1/space.ts
@@ -4,3 +4,24 @@ import { Router } from "express";
 export const spaceRouter = Router()
 
 
+spaceRouter.post("/", async (req, res) => {
+
+})
+
+
+spaceRouter.delete("/element", async (req, res) => {
+
+})
+
+
+
+spaceRouter.delete("/:spaceId", async(req, res) => {
+
+})
+
+
+spaceRouter.get("/all",  async (req, res) => {
+
+})
+
+
