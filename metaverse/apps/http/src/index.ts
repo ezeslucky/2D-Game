685b491cc92bd0ce32efb7554c1e954c11cc22b9
Lucky <lucky123@gmail.com>
2024-11-23 18:18:42 +0530

    http setup

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,9 +1,5 @@
-import express from "express";
-import { router } from "./routes/v1";
+import express from "express"
 
-const app = express();
-app.use(express.json())
+const app = express()
 
-app.use("/api/v1", router)
-
-app.listen(process.env.PORT || 3000)
+app.use("/api/v1")
